3c0af6d39d58b888aff559daa14e155c694332f6
Marco Celestino <marco.celestino@ovolab.com>
2022-09-27 15:54:56 +0200

    update

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-			name: "QuandoPasso", targets: ["QuandoPassoLib"])
+			name: "QuandoPasso", targets: ["QuandoPassoDependencies", "QuandoPassoLib"])
     ],
     dependencies: [
 		.package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.1"))
@@ -16,6 +16,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on
+		.target(
+			name: "QuandoPassoDependencies",
+			dependencies: ["QuandoPassoLib"]
+		),
 		.binaryTarget(
 			name: "QuandoPassoLib",
 			url: "https://api.github.com/repos/celes-ovolab/testSDK/releases/assets/79171950.zip",
---
+++ Sources/QuandoPassoDependencies/QuandoPassoDependencies.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Marco Celestino on 27/09/22.
+//
+
+import Foundation
